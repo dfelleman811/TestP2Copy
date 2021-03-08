10e4aced1e65ad61b6a83bb0378dcffd7549764f
phil <62857839+dfelleman811@users.noreply.github.com>
2021-03-08 18:35:59 -0500

    jenkins debugging-adding static resources

--- src/main/java/com/excape/app/P2CopyApplication.java
+++ src/main/java/com/excape/app/P2CopyApplication.java
@@ -3,6 +3,8 @@ package com.excape.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,11 +12,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("com.excape") // Annotation to provide location for packages
 @EnableJpaRepositories("com.excape.repositories") // Annotation specifically for Repo resource location
 @EntityScan("com.excape.beans") // Annotation to tell it where our entities are
-public class P2CopyApplication {
+public class P2CopyApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(P2CopyApplication.class, args);
 	}
 	
-		
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(P2CopyApplication.class);
+    }
 }
